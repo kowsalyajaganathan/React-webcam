f7fd3fdc0e35f8324a60185c6a5a638558f0ff2d
kowsalyajaganathan <kowsalyajaganathan@gmail.com>
2022-11-26 08:17:25 -0700

    updated

--- src/services/FacesService.js
+++ src/services/FacesService.js
@@ -2,15 +2,15 @@ export async function compareFaces() {
 
     try{
         console.log("in service");
-       /* const response = await fetch('https://5l4jeuu4ng.execute-api.us-east-1.amazonaws.com/dev/compareFaces');
+       const response = await fetch('https://5l4jeuu4ng.execute-api.us-east-1.amazonaws.com/dev/comparefaces');
         return await response.json(); 
-        */
-       const url ='https://5l4jeuu4ng.execute-api.us-east-1.amazonaws.com/dev/compareFaces'
+        
+     /*  const url ='https://5l4jeuu4ng.execute-api.us-east-1.amazonaws.com/dev/comparefaces'
 
         var request = new XMLHttpRequest();
 request.open("GET", url, false);
 request.send(null);
-console.log(request.responseText);
+console.log(request.responseText);*/
 
     }catch(error) {
         return [];
